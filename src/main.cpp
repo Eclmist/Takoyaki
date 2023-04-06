178f4427da65b1ae2224f62f59d45478d93caead
Samuel Huang <samuel.huanghaoyi@gmail.com>
2023-04-06 19:58:52 +0900

    Add system tray icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,104 @@
+/*
+    This file is part of Takoyaki, a custom screen share utility.
+
+    Copyright (c) 2020-2023 Samuel Huang - All rights reserved.
+
+    Takoyaki is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "Tako/includes/tako.h"
+
+#define WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
 #include <iostream>
+#include "resource.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#define NOTIFICATION_TRAY_ICON_MSG      (WM_USER + 0x100)
+#define NOTIFICATION_TRAY_UID           1
+#define IDM_EXIT                        100
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd)
 {
-    // Initialize the Taco library
-    Tako::TakoError result = Tako::Initialize();
-    if (result != Tako::TakoError::OK) {
-        std::cerr << "Failed to initialize Taco library: " << static_cast<int>(result) << std::endl;
-        return -1;
+    // Register the window class
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"TakoyakiWindowClass";
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindow(L"TakoyakiWindowClass", L"Takoyaki", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, NULL, NULL, hInstance, NULL);
+
+    // Add the icon to the system tray
+    NOTIFYICONDATA nid = { 0 };
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hwnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_USER;
+    nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+    lstrcpy(nid.szTip, L"Takoyaki");
+    Shell_NotifyIcon(NIM_ADD, &nid);
+
+    // Run the message loop
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-    // Start capturing the screen
-    result = Tako::StartCapture();
-    if (result != Tako::TakoError::OK) {
-        std::cerr << "Failed to start capturing screen: " << static_cast<int>(result) << std::endl;
-        Tako::Shutdown();
-        return -1;
+    // Remove the icon from the system tray
+    Shell_NotifyIcon(NIM_DELETE, &nid);
+
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_USER:
+        // Handle system tray messages here
+        if (LOWORD(lParam) == WM_RBUTTONUP)
+        {
+            // Show the context menu
+            POINT pt;
+            GetCursorPos(&pt);
+            HMENU hMenu = CreatePopupMenu();
+            AppendMenu(hMenu, MF_STRING, IDM_EXIT, L"Exit");
+            TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hwnd, NULL);
+        }
+        break;
+    case WM_COMMAND:
+        if (lParam == 0 && LOWORD(wParam) == IDM_EXIT)
+            PostQuitMessage(0);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
     }
-
-    // Share a specified area of the screen
-    Tako::TakoDisplayBuffer* buffer = nullptr;
-    result = Tako::GetBuffer(0, 0, 640, 480, &buffer);
-    if (result != Tako::TakoError::OK) {
-        std::cerr << "Failed to get screen buffer: " << static_cast<int>(result) << std::endl;
-        Tako::StopCapture();
-        Tako::Shutdown();
-        return -1;
-    }
-
-    // TODO: Share the buffer with others
-
-    // Stop capturing the screen and clean up the library
-    Tako::StopCapture();
-    Tako::Shutdown();
 
     return 0;
 }
